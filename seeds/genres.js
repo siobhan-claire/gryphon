f4ebf17828945ad884c9dc99964aa3167549c931
Siobhan O'Connor <Siobhy@MacBook-Pro-3.local>
2016-08-05 10:07:46 +1200

    created seeds

--- seeds/genres.js
+++ seeds/genres.js
@@ -1,13 +1,15 @@
 
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
+  return knex('genres').del()
     .then(function () {
       return Promise.all([
         // Inserts seed entries
-        knex('table_name').insert({id: 1, colName: 'rowValue1'}),
-        knex('table_name').insert({id: 2, colName: 'rowValue2'}),
-        knex('table_name').insert({id: 3, colName: 'rowValue3'})
+        knex('genres').insert({id: 1, name: 'Science Fiction', genre_id:1 }),
+        knex('genres').insert({id: 2, name: 'Poetry', genre_id:2 }),
+        knex('genres').insert({id: 3, name: 'Classics', genre_id:3 }),
+        knex('genres').insert({id: 3, name: 'Philosophy', genre_id:4 }),
+        knex('genres').insert({id: 3, name: 'Graphic Novels', genre_id:5 })
       ]);
     });
 };
