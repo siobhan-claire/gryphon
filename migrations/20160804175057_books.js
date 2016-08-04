42953aa5cfd424a7e047480ff6f9c389a4f54fed
Siobhan O'Connor <Siobhy@MacBook-Pro-3.local>
2016-08-05 11:09:35 +1200

    populated tables

--- migrations/20160804175057_books.js
+++ migrations/20160804175057_books.js
@@ -2,9 +2,10 @@ exports.up = function(knex, Promise) {
   return knex.schema.createTable('books', function (table) {
     table.increments('id').primary()
     table.string('title')
+    table.string('synopsis')
     table.integer('genre_id')
-    table.string('isbn')
     table.string('cover_art')
+    table.string('isbn')
   })
 };
 
--- seeds/genres.js
+++ seeds/genres.js
@@ -5,11 +5,11 @@ exports.seed = function(knex, Promise) {
     .then(function () {
       return Promise.all([
         // Inserts seed entries
-        knex('genres').insert({id: 1, name: 'Science Fiction', genre_id:1 }),
-        knex('genres').insert({id: 2, name: 'Poetry', genre_id:2 }),
-        knex('genres').insert({id: 3, name: 'Classics', genre_id:3 }),
-        knex('genres').insert({id: 3, name: 'Philosophy', genre_id:4 }),
-        knex('genres').insert({id: 3, name: 'Graphic Novels', genre_id:5 })
+        knex('genres').insert({id: 1, genre: 'Science Fiction', genre_id:1 }),
+        knex('genres').insert({id: 2, genre: 'Poetry', genre_id:2 }),
+        knex('genres').insert({id: 3, genre: 'Classics', genre_id:3 }),
+        knex('genres').insert({id: 4, genre: 'Philosophy', genre_id:4 }),
+        knex('genres').insert({id: 5, genre: 'Graphic Novels', genre_id:5 })
       ]);
     });
 };
